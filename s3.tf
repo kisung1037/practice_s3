7d7d8fe4486335068c929259284fad5edc04df62
kisung <kisung1037@gmail.com>
2022-12-31 16:00:03 +0900

    ..

--- s3.tf
+++ s3.tf
@@ -33,3 +33,14 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
   
 }
+
+terraform {
+  backend "s3" {
+    bucket          = "terraform-up-and-running-state"
+    key             = "global/s3/terraform.tfstate"
+    region          = "us-east-2"
+
+    dynamodb_table  = "terraform-up-and-running-locks"
+    encrypt         = true
+  }
+}
